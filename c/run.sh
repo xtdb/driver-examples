c4b721e1c81796431aa00c0a46ddc3844e3560b9
Jeremy Taylor <jdt@juxt.pro>
2025-02-24 22:22:09 +0000

    adjust C run.sh

--- c/run.sh
+++ c/run.sh
@@ -1,2 +1,4 @@
-gcc -o trades trades.c $(pkg-config --cflags --libs libpq)
+make
+# ...or if you want a one-liner instead of a Makefile:
+# gcc -o trades trades.c $(pkg-config --cflags --libs libpq)
 ./trades -h xtdb -d xtdb -p 5432
