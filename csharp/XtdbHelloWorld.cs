6028976ed6a2a706c9ce17d7daf37f87b3caf7d4
Jeremy Taylor <jdt@juxt.pro>
2025-05-09 15:00:23 +0100

    add XTQL to csharp example

--- csharp/XtdbHelloWorld.cs
+++ csharp/XtdbHelloWorld.cs
@@ -9,14 +9,26 @@ class XtdbOdbcTest
         using var connection = new OdbcConnection(connectionString);
         connection.Open();
 
-        using var command = connection.CreateCommand();
-
-        command.CommandText = "SELECT xt.version() AS x";
-        using var reader = command.ExecuteReader();
-
-        while (reader.Read())
+        using (var insertCommand = connection.CreateCommand())
         {
-            Console.WriteLine($"x = {reader["x"]}");
+            insertCommand.CommandText = "INSERT INTO foo (_id, bar) VALUES (1, 'baz')";
+            insertCommand.ExecuteNonQuery();
+        }
+
+        using (var queryCommand = connection.CreateCommand())
+        {
+            queryCommand.CommandText = "SELECT xt.version() AS v, * FROM (XTQL $XT$ (from :foo [*]) $XT$) q";
+            using var reader = queryCommand.ExecuteReader();
+            while (reader.Read())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    var name = reader.GetName(i);
+                    var value = reader.IsDBNull(i) ? "NULL" : reader.GetValue(i);
+                    Console.Write($"{name} = {value}; ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
