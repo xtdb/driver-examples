87aef5f4fab635a34a816b3968e782f938ea9d29
Jeremy Taylor <jdt@juxt.pro>
2025-05-31 17:05:25 +0100

    add transit-js example for node

--- node/index.mjs
+++ node/index.mjs
@@ -1,6 +1,9 @@
 "use strict";
 
 import postgres from "postgres";
+import transit from "transit-js";
+
+const writer = transit.writer("json");
 
 const sql = postgres({
   host: "xtdb",
@@ -17,14 +20,31 @@ const sql = postgres({
   },
 });
 
+const profile_record = {
+  _id: 3,
+  name: 'bob',
+  age: 42,
+  location: {
+    city: "Berlin",
+    zip: "10115"
+  },
+  misc: ['a', 'b', {c: ['d']}],
+  last_contacted: new Date("2025-05-21T09:47:09.892Z")
+};
+
+const transit_oid = 16384;
+
 async function main() {
   try {
     // Insert data into the "users" table
     await sql`INSERT INTO users (_id, name) VALUES (${sql.typed.int(1)}, 'James'), (${sql.typed.int(2)}, 'Jeremy')`;
 
+    // Insert transit-js record into the "users" table
+    await sql`INSERT INTO users RECORDS ${sql.typed(writer.write(profile_record), transit_oid)}`;
+
     // Fetch data from the "users" table
-    const users = await sql`SELECT _id, name FROM users`;
-    console.log([...users]); // => [{_id: 1, name: "James"}, {_id: 2, name: "Jeremy"}]
+    const users = await sql`SELECT * FROM users`;
+    console.dir([...users], { depth: null });
   } catch (err) {
     console.error("Error occurred:", err);
   } finally {
