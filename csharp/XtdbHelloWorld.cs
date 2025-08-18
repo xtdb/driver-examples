51dea330cd5c51607c2bab642140794ca06e9472
Rhys Williams <5460583+xanth@users.noreply.github.com>
2025-08-18 20:17:01 +1000

    Replace C# Odbc example with Npgsql (#4)
    * Replace C# Odbc example with Npgsql
    
    * Fixed comment

--- csharp/XtdbHelloWorld.cs
+++ csharp/XtdbHelloWorld.cs
@@ -1,30 +1,54 @@
 using System;
-using System.Data.Odbc;
+using System.Threading.Tasks;
+using Npgsql;
 
 class XtdbOdbcTest
 {
-    static void Main()
+    static async Task Main()
     {
-        var connectionString = "Driver={PostgreSQL Unicode};Server=xtdb;Port=5432;Database=xtdb;Uid=xtdb;Pwd=xtdb;";
-        using var connection = new OdbcConnection(connectionString);
-        connection.Open();
+        var connectionString = "Host=xtdb;Port=5432;Database=xtdb;Username=xtdb;Password=xtdb;";
 
-        using (var insertCommand = connection.CreateCommand())
+        var dataSourceBuilder = new NpgsqlDataSourceBuilder(connectionString);
+        
+        // Required for XTDB support
+        dataSourceBuilder.ConfigureTypeLoading(sb => {
+            sb.EnableTypeLoading(false);
+            sb.EnableTableCompositesLoading(false);
+        });
+
+        var dataSource = dataSourceBuilder.Build();
+        var connection = await dataSource.OpenConnectionAsync();
+
+        await using (var insertCommand = connection.CreateCommand())
         {
-            insertCommand.CommandText = "INSERT INTO foo (_id, bar) VALUES (1, 'baz')";
-            insertCommand.ExecuteNonQuery();
+            insertCommand.Parameters.Add(new NpgsqlParameter()).Value = "baz";
+
+            insertCommand.CommandText = "INSERT INTO foo (_id, bar) VALUES (1, ?)";
+            await insertCommand.ExecuteNonQueryAsync();
+        }
+        
+        await using (var queryCommand = connection.CreateCommand())
+        {
+            queryCommand.CommandText = """
+                                       XTQL $$
+                                         (from :foo [bar])
+                                       $$
+                                       """;
+            var value = await queryCommand.ExecuteScalarAsync();
+            
+            Console.WriteLine($"foo:bar = {value}; ");
         }
 
-        using (var queryCommand = connection.CreateCommand())
+        await using (var queryCommand = connection.CreateCommand())
         {
             queryCommand.CommandText = "SELECT xt.version() AS v, * FROM (XTQL $XT$ (from :foo [*]) $XT$) q";
-            using var reader = queryCommand.ExecuteReader();
-            while (reader.Read())
+            await using var reader = await queryCommand.ExecuteReaderAsync();
+            while (await reader.ReadAsync())
             {
                 for (int i = 0; i < reader.FieldCount; i++)
                 {
                     var name = reader.GetName(i);
-                    var value = reader.IsDBNull(i) ? "NULL" : reader.GetValue(i);
+                    var value = await reader.IsDBNullAsync(i) ? "NULL" : await reader.GetFieldValueAsync<object>(i);
                     Console.Write($"{name} = {value}; ");
                 }
                 Console.WriteLine();
