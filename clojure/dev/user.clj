34a9e98bc831525b795e3bd1909285aa99030200
Jeremy Taylor <jdt@juxt.pro>
2025-09-21 21:00:34 +0100

    use official XTDB Clojure API for the Clojure user.clj example

--- clojure/dev/user.clj
+++ clojure/dev/user.clj
@@ -1,18 +1,18 @@
 (ns user
-  (:require [next.jdbc :as jdbc]
-            [xtdb.api :as xt]
-            [xtdb.next.jdbc :as xt-jdbc]))
+  (:require [xtdb.api :as xt]))
 
-(defn get-conn []
-  (jdbc/get-connection "jdbc:xtdb://xtdb:5432/xtdb?options=-c%20TimeZone=UTC"))
+(defn get-client []
+  (xt/client {:host "xtdb"
+              :port 5432
+              :user "xtdb"}))
 
 (defn -main [& _args]
-  (with-open [conn (get-conn)]
+  (let [client (get-client)]
 
     ;; using the REPL? you probably want this:
-    ;; (def conn (get-conn))
+    ;; (def client (get-client))
 
-    (jdbc/execute! conn ["INSERT INTO users RECORDS {_id: 'jms', first_name: 'James'}"])
+    (xt/execute-tx client ["INSERT INTO users RECORDS {_id: 'jms', first_name: 'James'}"])
 
     (def m {:xt/id "joe"
             :first-name "Joe" ;; string
@@ -28,55 +28,61 @@
             :my-time (java.time.LocalTime/of 22 15 04 1237) ;; also #xt/time "22:15:04.1237"
             :my-duration (java.time.Duration/parse "PT1H3M5.533S") ;; also #xt/duration "PT1H3M5.533S"
             :my-interval #xt/interval "P163DT12H"
-            :my-period #xt/tstz-range [#xt/zdt "2025-01-01Z"  #xt/zdt "2025-06-12Z"] ;; upper value can also be nil (which is used extensively for _valid_time and _system_time periods)
+            #_#_:my-period #xt/tstz-range [#xt/zdt "2025-01-01Z"  #xt/zdt "2025-06-12Z"] ;; upper value can also be nil (which is used extensively for _valid_time and _system_time periods) ;; blocked by https://github.com/xtdb/xtdb/issues/4379
             :my-uri #xt/uri "https://xtdb.com"
             :my-uuid #uuid "97a392d5-5e3f-406f-9651-a828ee79b156"})
 
-    (jdbc/execute! conn ["INSERT INTO users RECORDS ?" m])
+    ;; Insert with parameterized SQL
+    (xt/execute-tx client [["INSERT INTO users RECORDS ?" m]])
 
-    ;; everything above can roundtrip when using the provided builder-fn
+    ;; Insert with parameterized SQL batches
+    (xt/execute-tx client [[:sql "INSERT INTO users RECORDS ?" [m]]])
+
+    ;; everything above can be roundtripped through XTDB
+    ;; implemented using Apache Arrow types internally (but also available via FlightSQL!)
     ;; such that this expression should eval to true:
-    (= m (first (jdbc/execute! conn ["SELECT * FROM users WHERE _id = 'joe'"]
-                               {:builder-fn xt-jdbc/builder-fn})))
+    (= m (first (xt/q client ["SELECT * FROM users WHERE _id = 'joe'"])))
 
     ;; should you see `org.postgresql.util.PSQLException: ERROR: Relevant table schema has changed since preparing query, please prepare again`
     ;; or recently inserted columns you'd now expect to see via `SELECT *` are missing
-    ;; ...then simply re-def the conn and try again:
-    ;; (def conn (get-conn))
+    ;; ...then simply re-def the client and try again:
+    ;; (def client (get-client))
 
-    (jdbc/execute! conn
-                   ["PATCH INTO users RECORDS ?" {:xt/id "joe", :likes "chocolate"}])
+    (xt/execute-tx client
+                  [["PATCH INTO users RECORDS ?" {:xt/id "joe", :likes "chocolate"}]])
 
-    (prn (jdbc/execute! conn ["SELECT * FROM users"] {:builder-fn xt-jdbc/builder-fn}))
+    (prn (xt/q client "SELECT * FROM users"))
 
     ;; access temporal columns
-    (jdbc/execute! conn ["SELECT *, _valid_time, _system_time FROM users"] {:builder-fn xt-jdbc/builder-fn})
+    (prn (xt/q client "SELECT *, _valid_time, _system_time FROM users"))
 
     ;; the "database as a value" semantics are achieved with _system_time columns and SNAPSHOT_TIME
-    (jdbc/execute! conn ["SETTING SNAPSHOT_TIME TO '2020-01-01Z' SELECT * FROM users"] {:builder-fn xt-jdbc/builder-fn})
+    (prn (xt/q client "SETTING SNAPSHOT_TIME TO '2020-01-01Z' SELECT * FROM users"))
 
     ;; users can time-travel with DEFAULT VALID_TIME
-    (jdbc/execute! conn ["SETTING DEFAULT VALID_TIME AS OF '2027-01-01Z' SELECT * FROM users"] {:builder-fn xt-jdbc/builder-fn})
+    (prn (xt/q client "SETTING DEFAULT VALID_TIME AS OF '2027-01-01Z' SELECT * FROM users"))
 
     ;; or per-table filters
-    (jdbc/execute! conn ["SELECT * FROM users FOR VALID_TIME AS OF '2027-01-01Z'"] {:builder-fn xt-jdbc/builder-fn})
+    (prn (xt/q client "SELECT * FROM users FOR VALID_TIME AS OF '2027-01-01Z'"))
 
     ;; transactions are reified
-    (jdbc/execute! conn ["SELECT * FROM xt.txs"] {:builder-fn xt-jdbc/builder-fn})
+    (prn (xt/q client "SELECT * FROM xt.txs"))
 
     ;; use XTQL within SQL
-    (jdbc/execute! conn [(format "SELECT * FROM (XTQL $$ %s $$) xtql_res WHERE _id = 'joe'"
-                                 '(-> (from :users [first-name xt/id])
-                                      (with {:full-name (concat first-name " Smith")})))]
-                   {:builder-fn xt-jdbc/builder-fn})
-  ))
+    (prn (xt/q client (format "SELECT * FROM (XTQL $$ %s $$) xtql_res WHERE _id = 'joe'"
+                              '(-> (from :users [first-name xt/id])
+                                   (with {:full-name (concat first-name " Smith")})))))))
 
 (comment
   (-main)
 
-  ;; to explore the Sakila dataset using datafy/nav tooling
-  (tap> (jdbc/execute! (get-conn) ["select * from inventory"]
-                       {:schema-opts {:pk "_id"}
-                        :builder-fn xt-jdbc/builder-fn}))
+  (require '[next.jdbc :as jdbc])
+
+  ;; To explore the Sakila dataset using datafy/nav tooling with next.jdbc
+  (defn get-jdbc-conn []
+    (jdbc/get-connection "jdbc:xtdb://xtdb:5432/xtdb?options=-c%20TimeZone=UTC"))
+
+  (tap> (jdbc/execute! (get-jdbc-conn) ["select * from inventory"]
+                       {:schema-opts {:pk "_id"}}))
 
   )
