abeba4f4e828b12de50b359015a9b93f8edd6f75
Jeremy Taylor <jdt@juxt.pro>
2025-10-02 11:40:19 +0100

    fix playground command

--- .devcontainer/run-xtdb-dev.sh
+++ .devcontainer/run-xtdb-dev.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/xtdb/xtdb
 cd xtdb
-./gradlew run playground --port 5433
+# playground mode will accept any database name, creating it dynamically if needed
+./gradlew run --args="playground --port 5433"
