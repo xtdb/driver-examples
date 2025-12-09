5c4005384ce24352f6410345f96543b7eb501282
Jeremy Taylor <jdt@juxt.pro>
2025-12-09 17:56:36 +0000

    add 'app' to up.sh (again)

--- .devcontainer/up.sh
+++ .devcontainer/up.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+# Useful script for local development setup
+
 # Pull latest xtdb image
 docker-compose pull xtdb
 
 # Start infrastructure services in detached mode (will stay running after ctrl-c)
-docker-compose up -d init-volumes garage garage-webui init-garage redpanda metabase
+docker-compose up -d init-volumes garage garage-webui init-garage redpanda metabase app
 
 # Start xtdb in foreground (ctrl-c will stop only this)
 docker-compose up xtdb
